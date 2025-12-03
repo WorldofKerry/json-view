3f6964ae17178680825c35c3682fd1524b737623
Pavlo Hrabovets <pgrabovets3056@gmail.com>
2025-12-03 04:19:53 +0200

    update dist

--- dist/jsonview.js
+++ dist/jsonview.js
@@ -1,106 +1,135 @@
-function c(e) {
+function a(e) {
   return Array.isArray(e) ? "array" : e === null ? "null" : typeof e;
 }
-function u(e) {
+function f(e) {
   return document.createElement(e);
 }
-function D(e, n, t) {
-  return e.addEventListener(n, t), () => e.removeEventListener(n, t);
+function I(e, t, n) {
+  return e.addEventListener(t, n), () => e.removeEventListener(t, n);
 }
-function g(e) {
+function T(e) {
   e.parentNode.removeChild(e);
 }
-const r = {
+const i = {
   HIDDEN: "hidden",
   CARET_ICON: "caret-icon",
   CARET_RIGHT: "fa-caret-right",
   CARET_DOWN: "fa-caret-down",
-  ICON: "fas"
+  ICON: "fas",
+  CONTAINER: "json-container"
 };
-function I(e = {}) {
-  const { key: n, size: t } = e;
+function m(e = {}) {
+  const { key: t, size: n, isExpanded: s = !1 } = e;
   return `
     <div class="line">
-      <div class="caret-icon"><i class="fas fa-caret-right"></i></div>
-      <div class="json-key">${n}</div>
-      <div class="json-size">${t}</div>
+      <div class="caret-icon"><i class="fas ${s ? i.CARET_DOWN : i.CARET_RIGHT}"></i></div>
+      <div class="json-key">${t}</div>
+      <div class="json-size">${n}</div>
     </div>
   `;
 }
-function T(e = {}) {
-  const { key: n, value: t, type: i } = e;
+function x(e = {}) {
+  const { key: t, value: n, type: s } = e;
   return `
     <div class="line">
       <div class="empty-icon"></div>
-      <div class="json-key">${n}</div>
+      <div class="json-key">${t}</div>
       <div class="json-separator">:</div>
-      <div class="json-value json-${i}">${t}</div>
+      <div class="json-value ${s}">${n}</div>
     </div>
   `;
 }
 function j() {
-  const e = u("div");
-  return e.className = "json-container", e;
+  const e = f("div");
+  return e.className = i.CONTAINER, e;
 }
-function o(e) {
-  e.children.forEach((n) => {
-    n.el.classList.add(r.HIDDEN), n.isExpanded && o(n);
-  });
-}
-function f(e) {
-  e.children.forEach((n) => {
-    n.el.classList.remove(r.HIDDEN), n.isExpanded && f(n);
+function p(e) {
+  e.children.forEach((t) => {
+    t.el && t.el.classList.add(i.HIDDEN), t.isExpanded && p(t);
   });
 }
 function d(e) {
-  if (e.children.length > 0) {
-    const n = e.el.querySelector("." + r.ICON);
-    n && n.classList.replace(r.CARET_RIGHT, r.CARET_DOWN);
-  }
-}
-function p(e) {
-  if (e.children.length > 0) {
-    const n = e.el.querySelector("." + r.ICON);
-    n && n.classList.replace(r.CARET_DOWN, r.CARET_RIGHT);
-  }
-}
-function y(e) {
-  e.isExpanded ? (e.isExpanded = !1, p(e), o(e)) : (e.isExpanded = !0, d(e), f(e));
-}
-function m(e) {
-  let n = u("div");
-  const t = (l) => {
-    const a = l.children.length;
-    return l.type === "array" ? `[${a}]` : l.type === "object" ? `{${a}}` : null;
-  };
-  if (e.children.length > 0) {
-    n.innerHTML = I({
-      key: e.key,
-      size: t(e)
-    });
-    const l = n.querySelector("." + r.CARET_ICON);
-    e.dispose = D(l, "click", () => y(e));
-  } else
-    n.innerHTML = T({
-      key: e.key,
-      value: e.value === "" ? '""' : e.value,
-      type: e.value === "{}" ? "object" : typeof e.value
-    });
-  const i = n.children[0];
-  return e.parent !== null && i.classList.add(r.HIDDEN), i.style = "margin-left: " + e.depth * 18 + "px;", i;
-}
-function s(e, n) {
-  n(e), e.children.length > 0 && e.children.forEach((t) => {
-    s(t, n);
+  e.children.forEach((t) => {
+    t.el && t.el.classList.remove(i.HIDDEN), t.isExpanded && d(t);
   });
 }
-function v(e = {}) {
-  const n = (i) => c(i) === "object" && Object.keys(i).length === 0;
-  let t = e.hasOwnProperty("value") ? e.value : null;
-  return n(t) && (t = "{}"), {
+function y(e) {
+  if (e.children.length > 0 && e.el) {
+    const t = e.el.querySelector("." + i.ICON);
+    t && t.classList.replace(i.CARET_RIGHT, i.CARET_DOWN);
+  }
+}
+function E(e) {
+  if (e.children.length > 0 && e.el) {
+    const t = e.el.querySelector("." + i.ICON);
+    t && t.classList.replace(i.CARET_DOWN, i.CARET_RIGHT);
+  }
+}
+function v(e) {
+  e.isExpanded ? (e.isExpanded = !1, E(e), p(e)) : (e.isExpanded = !0, y(e), d(e));
+}
+function O(e) {
+  let t = f("div");
+  const n = (r) => {
+    const o = r.children.length;
+    return r.type === "array" ? `[${o}]` : r.type === "object" ? `{${o}}` : null;
+  }, s = (r) => {
+    switch (r.type) {
+      case "string":
+        return r.value ? `${r.value}` : '""';
+      case "number":
+      case "boolean":
+      case "null":
+        return `${r.value}`;
+      case "array":
+        return `[${r.children.length}]`;
+      case "object":
+        return `{${r.children.length}}`;
+      default:
+        return `${r.value}`;
+    }
+  }, u = (r) => {
+    switch (r.type) {
+      case "string":
+      case "number":
+      case "boolean":
+      case "null":
+      case "array":
+      case "object":
+        return `json-${r.type}`;
+      default:
+        return `json-${typeof r.value}`;
+    }
+  };
+  if (e.children.length > 0) {
+    t.innerHTML = m({
+      key: e.key,
+      size: n(e),
+      isExpanded: e.isExpanded
+    });
+    const r = t.querySelector("." + i.CARET_ICON);
+    e.dispose = I(r, "click", () => v(e));
+  } else
+    t.innerHTML = x({
+      key: e.key,
+      value: s(e),
+      type: u(e)
+    });
+  const c = t.children[0];
+  return e.parent !== null && (e.isExpanded ? c.classList.remove(i.HIDDEN) : c.classList.add(i.HIDDEN)), c.style = "margin-left: " + e.depth * 18 + "px;", c;
+}
+function l(e, t) {
+  t(e), e.children.length > 0 && e.children.forEach((n) => {
+    l(n, t);
+  });
+}
+function h(e = {}) {
+  const t = (s) => a(s) === "object" && Object.keys(s).length === 0;
+  let n = e.hasOwnProperty("value") ? e.value : null;
+  return t(n) && (n = "{}"), {
     key: e.key || null,
     parent: e.parent || null,
-    value: t,
+    value: n,
     isExpanded: e.isExpanded || !1,
     type: e.type || null,
     children: e.children || [],
@@ -109,73 +138,73 @@ function v(e = {}) {
     dispose: null
   };
 }
-function E(e, n) {
+function N(e, t) {
   if (typeof e == "object")
-    for (let t in e) {
-      const i = v({
-        value: e[t],
-        key: t,
-        depth: n.depth + 1,
-        type: c(e[t]),
-        parent: n
+    for (let n in e) {
+      const s = h({
+        value: e[n],
+        key: n,
+        depth: t.depth + 1,
+        type: a(e[n]),
+        parent: t
       });
-      n.children.push(i), E(e[t], i);
+      t.children.push(s), N(e[n], s);
     }
 }
-function h(e) {
+function C(e) {
   return typeof e == "string" ? JSON.parse(e) : e;
 }
-function N(e) {
-  const n = h(e), t = v({
-    value: n,
-    key: c(n),
-    type: c(n)
+function g(e) {
+  const t = C(e), n = h({
+    value: t,
+    key: a(t),
+    type: a(t)
   });
-  return E(n, t), t;
+  return N(t, n), n;
 }
-function k(e, n) {
-  const t = h(e), i = N(t);
-  return C(i, n), i;
+function R(e, t) {
+  const n = C(e), s = g(n);
+  return D(s, t), s;
 }
-function C(e, n) {
-  const t = j();
-  s(e, function(i) {
-    i.el = m(i), t.appendChild(i.el);
-  }), n.appendChild(t);
+function D(e, t) {
+  const n = j();
+  l(e, function(s) {
+    s.el = O(s), n.appendChild(s.el);
+  }), t.appendChild(n);
 }
-function x(e) {
-  s(e, function(n) {
-    n.el.classList.remove(r.HIDDEN), n.isExpanded = !0, d(n);
-  });
-}
-function O(e) {
-  s(e, function(n) {
-    n.isExpanded = !1, n.depth > e.depth && n.el.classList.add(r.HIDDEN), p(n);
+function $(e) {
+  l(e, function(t) {
+    t.el && t.el.classList.remove(i.HIDDEN), t.isExpanded = !0, y(t);
   });
 }
-function R(e) {
-  s(e, (n) => {
-    n.dispose && n.dispose();
-  }), g(e.el.parentNode);
+function k(e) {
+  l(e, function(t) {
+    t.isExpanded = !1, t.depth > e.depth && t.el && t.el.classList.add(i.HIDDEN), E(t);
+  });
+}
+function A(e) {
+  l(e, (t) => {
+    t.dispose && t.dispose();
+  }), T(e.el.parentNode);
 }
 const H = {
-  toggleNode: y,
-  render: C,
-  create: N,
-  renderJSON: k,
-  expand: x,
-  collapse: O,
-  traverse: s,
-  destroy: R
+  toggleNode: v,
+  render: D,
+  create: g,
+  renderJSON: R,
+  expand: $,
+  collapse: k,
+  traverse: l,
+  destroy: A
 };
 export {
-  O as collapse,
-  N as create,
+  k as collapse,
+  g as create,
   H as default,
-  R as destroy,
-  x as expand,
-  C as render,
-  k as renderJSON,
-  y as toggleNode,
-  s as traverse
+  A as destroy,
+  $ as expand,
+  D as render,
+  R as renderJSON,
+  v as toggleNode,
+  l as traverse
 };
